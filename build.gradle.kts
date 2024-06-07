402d394d45ea932fc91b5fd608f14b61875370d7
Rishon <rishonjaffe9@gmail.com>
2024-06-07 15:23:01 +0300

    fix: maven-publish creds

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "2.0.0"
     id("io.papermc.paperweight.userdev") version "1.7.1"
+    id("maven-publish")
 }
 
 group = "systems.rishon"
@@ -16,4 +17,28 @@ dependencies {
 
 kotlin {
     jvmToolchain(21)
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "seladevelopment-repo"
+            url = uri("https://repo.rishon.systems/releases")
+            credentials {
+                username = System.getenv("MAVEN_NAME")
+                password = System.getenv("MAVEN_SECRET")
+            }
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "systems.rishon"
+            artifactId = "selautils"
+            version = "1.0.0"
+            from(components["java"])
+        }
+    }
 }
