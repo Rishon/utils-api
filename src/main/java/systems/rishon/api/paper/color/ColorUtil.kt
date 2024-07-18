00c39ee92b6c39f81ab2ef89b1e6d0e5e296dd7d
Rishon <rishonjaffe9@gmail.com>
2024-07-18 23:33:57 +0300

    feat: wrap hex message as minimessage component

--- src/main/java/systems/rishon/api/paper/color/ColorUtil.kt
+++ src/main/java/systems/rishon/api/paper/color/ColorUtil.kt
@@ -21,6 +21,25 @@ object ColorUtil {
         return mm.deserialize(color.color + string).decoration(TextDecoration.ITALIC, false)
     }
 
+    @JvmStatic
+    fun wrapMini(msg: String): Component {
+        val pattern = "#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})"
+        val r = Regex(pattern)
+        val sb = StringBuffer()
+        val m = r.findAll(msg)
+        var lastIndex = 0
+
+        for (match in m) {
+            sb.append(msg, lastIndex, match.range.first)
+            sb.append("<${match.value}>")
+            lastIndex = match.range.last + 1
+        }
+
+        sb.append(msg.substring(lastIndex))
+        return mm.deserialize(sb.toString())
+    }
+
+
     // Legacy Hex (ex. #FFFFFF)
     @JvmStatic
     @Deprecated("Use translate instead")
